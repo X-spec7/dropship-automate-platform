bd9fbe3bfb94b5131a83d10e696b7cac0aa47760
X-spec7 <mercury.spec77@gmail.com>
2024-08-06 00:21:56 +0900

    chore: update content and button style

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -12,10 +12,10 @@ const Contact = () => {
               "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Need Help?&nbsp; Contact Us!
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
-                Our support team will get back to you ASAP via email.
+                We will get back to you ASAP via email.
               </p>
               <form>
                 <div className="-mx-4 flex flex-wrap">
@@ -66,8 +66,8 @@ const Contact = () => {
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                      Submit Ticket
+                    <button className="rounded-lg bg-primary px-12 py-3 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                      Contact
                     </button>
                   </div>
                 </div>
